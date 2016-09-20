1871817b861d60c1eb51baae9f772ce35239166a
antoine drabble <antoine drabble>
2016-09-20 19:06:10 +0200

    Create 2nd projet and send/receive UDP packets over multicast and point to point

---
+++ Master/src/Main.java
@@ -0,0 +1,73 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        System.out.println("Hello World!");
+
+        int nbSlaves = 1;
+
+        InetAddress groupe = InetAddress.getByName("228.5.6.7");
+        MulticastSocket multicastSocket = new MulticastSocket(4446);
+
+        byte[] tampon = new byte[8];
+        DatagramSocket pointToPointSocket = new DatagramSocket(4444);
+
+        int cnt = 0;
+        while (cnt < 1000) {
+
+            cnt++;
+
+            /*SimpleDateFormat sdf = new SimpleDateFormat("MM dd, yyyy HH:mm");
+            Date result = new Date(System.currentTimeMillis());
+            result = new Date(System.currentTimeMillis()+decalage);*/
+
+            // Retrieve the current time and send it on the multicast
+            long currentTime = System.currentTimeMillis();
+            byte[] bytes = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(currentTime).array();
+
+            DatagramPacket paquet = new DatagramPacket(bytes, bytes.length, groupe, 4445);
+
+            multicastSocket.send(paquet);
+
+            System.out.println("Current time envoye : " + currentTime);
+
+            // Receive every response from the slaves
+            LinkedList<Long> decalages = new LinkedList<Long>();
+            for(int i = 0; i < nbSlaves; i++){
+                DatagramPacket paquet2 = new DatagramPacket(tampon, tampon.length);
+                pointToPointSocket.receive(paquet2);
+                long valeurRecue = 0;
+                byte[] byteRecu = paquet2.getData();
+                for (int j = 0; j < byteRecu.length; j++)
+                {
+                    valeurRecue = (valeurRecue << 8) + (byteRecu[j] & 0xff);
+                }
+                System.out.println("Decalage recue : " + valeurRecue);
+                decalages.push(valeurRecue);
+            }
+
+            // Calculate maximum decalage and send it
+            long decalageMax = Collections.max(decalages);
+            byte[] bytes2 = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(decalageMax + System.currentTimeMillis()).array();
+
+            DatagramPacket paquet2 = new DatagramPacket(bytes2, bytes2.length, groupe, 4445);
+
+            multicastSocket.send(paquet2);
+            System.out.println("Decalage max recu : " + decalageMax);
+
+
+            Thread.sleep(10000);
+        }
+        multicastSocket.close();
+        pointToPointSocket.close();
+        //ms.leaveGroup(adrLocale);
+    }
+}
---
+++ Slave/src/Main.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.MulticastSocket;
+import java.nio.ByteBuffer;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+        System.out.println("Hello World!");
+
+        long decalage = 0;
+
+        byte[] tampon = new byte[8];
+
+        MulticastSocket multicastSocket = new MulticastSocket(4445);
+        InetAddress groupe = InetAddress.getByName("228.5.6.7");
+        multicastSocket.joinGroup(groupe);
+
+        DatagramSocket pointToPointSocket = new DatagramSocket();
+
+        int cnt = 0;
+        while (cnt < 1000) {
+            // Retrieve master current time
+            DatagramPacket paquet = new DatagramPacket(tampon, tampon.length);
+            multicastSocket.receive(paquet);
+
+            long valeurRecue = 0;
+            byte[] byteRecu = paquet.getData();
+            for (int i = 0; i < byteRecu.length; i++)
+            {
+                valeurRecue = (valeurRecue << 8) + (byteRecu[i] & 0xff);
+            }
+            System.out.println("Valeur recue : " + valeurRecue);
+
+            // Calculate new decalage
+            decalage = System.currentTimeMillis() - valeurRecue;
+
+            // Send new decalage
+            byte[] tampon2 = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(decalage).array();
+            InetAddress address = InetAddress.getByName("127.0.0.1");
+            DatagramPacket paquet2 = new DatagramPacket(tampon2, tampon2.length, address,  4444);
+            pointToPointSocket.send(paquet2);
+
+            System.out.println("Decalage envoye : " + decalage);
+
+            // Receive new decalage
+            DatagramPacket paquet3 = new DatagramPacket(tampon, tampon.length);
+            multicastSocket.receive(paquet3);
+
+            long valeurRecue2 = 0;
+            byte[] byteRecu2 = paquet3.getData();
+            for (int i = 0; i < byteRecu2.length; i++)
+            {
+                valeurRecue = (valeurRecue2 << 8) + (byteRecu2[i] & 0xff);
+            }
+            System.out.println("Valeur recue : " + valeurRecue2);
+            decalage = valeurRecue2 - System.currentTimeMillis();
+
+            cnt++;
+        }
+
+        multicastSocket.leaveGroup(groupe);
+        multicastSocket.close();
+        pointToPointSocket.close();
+    }
+}
