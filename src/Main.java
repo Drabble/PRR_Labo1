84d8795f3020e856e39d097a5c5c73a607f6608a
simon-baehler <simon.baehler@heig-vd.ch>
2016-09-20 14:23:59 +0200

    horloge local

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello World!");
+        long decalage = 10000;
+        Calendar cal_0 = Calendar.getInstance();
+        SimpleDateFormat sdf = new SimpleDateFormat("MM dd, yyyy HH:mm");
+        Date result = new Date(System.currentTimeMillis());
+        while (true) {
+            Thread.sleep(1000);
+            result = new Date(System.currentTimeMillis()+decalage);
+            System.out.println(result);
+        }
     }
 }
