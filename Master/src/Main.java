bd564229d69918b2ecd3ba02e1bf2c80bf9a7976
simon-baehler <simon-baehler@heig-vd.ch>
2016-10-04 13:52:49 +0200

    comments and rename

--- Master/src/Main.java
+++ Master/src/Main.java
@@ -2,7 +2,10 @@
  * Project : Labo 1 PRR
  * Author : Antoine Drabble & Simon Baehler
  * Date : 22.09.2016
- * Description : Slave
+ * Description : Master
+ * This program play the role of the master, each 10 second the master demand the time of it subscribers between the
+ * master time and the subscriber time. After that he found the bigest value and send it to the salves (the value is
+ * not absolute, we only want to go forward in the time)
  */
 
 import java.io.IOException;
@@ -14,26 +17,31 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         int nbSlaves = 1; // Number of slaves to synchronise
+        final String multicastAddress = "228.5.6.7";
+        final int PointToPointPort = 4444 ;
+        final int datagramPacketPort = 4445;
+        final int multicastPort = 4446 ;
+        final String networkInterface = "wlan0";
 
         // Multicast socket to communicate with slaves
-        InetAddress multicastGroup = InetAddress.getByName("228.5.6.7");
-        MulticastSocket multicastSocket = new MulticastSocket(4446);
+        InetAddress multicastGroup = InetAddress.getByName(multicastAddress);
+        MulticastSocket multicastSocket = new MulticastSocket(multicastPort);
         // Specify the network interface if it is not choosing the right one by default
-        multicastSocket.setNetworkInterface(NetworkInterface.getByName("wlan0"));
+        multicastSocket.setNetworkInterface(NetworkInterface.getByName(networkInterface));
         // Join group was not necessary on my home lan
         //multicastSocket.joinGroup(multicastGroup);
 
         // Point to point socket to receive message from slaves
-        DatagramSocket pointToPointSocket = new DatagramSocket(4444);
+        DatagramSocket pointToPointSocket = new DatagramSocket(PointToPointPort);
 
-        // Date fo
+        // Date
         SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss.SSS");
 
         while (true) {
             // Retrieve the current time and send it on the multicast
             long currentTime = System.currentTimeMillis();
             byte[] longBuffer = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(currentTime).array();
-            DatagramPacket timePacket = new DatagramPacket(longBuffer, longBuffer.length, multicastGroup, 4445);
+            DatagramPacket timePacket = new DatagramPacket(longBuffer, longBuffer.length, multicastGroup, datagramPacketPort);
             multicastSocket.send(timePacket);
             System.out.println("Current time sent : " + sdf.format(new Date(currentTime)));
 
@@ -64,7 +72,14 @@ public class Main {
         //pointToPointSocket.close();
     }
 
-
+    /**
+     * name : bytesToLong
+     * Author : Antoine Drabble & Simon Baehler
+     * Date : 22.09.2016
+     * Description : transforme a data of bytes to a long
+     * param : byte[] bytes - data of bytes
+     * out : long - value of the data of bytes in a long format
+     */
     private static long bytesToLong(byte[] bytes){
         long result = 0;
         for (int i = 0; i < bytes.length; i++)
--- Slave/src/Main.java
+++ Slave/src/Main.java
@@ -2,7 +2,10 @@
  * Projet : Labo 1 PRR
  * Auteur : Antoine Drabble & Simon Baehler
  * Date : 22.09.2016
- * Description : Master
+ * Description : Slave
+ *
+ * This program receive the demand of the master to send him its time after that he receive the max value of all salves
+ * who subscribed to the master then he set its time to this value (master time + shift max ) - salve time
  */
 
 import java.io.IOException;
@@ -15,11 +18,16 @@ public class Main {
     public static void main(String[] args) throws InterruptedException, IOException {
         long shift = 0; // Current time shift
 
+        final String multicastAddress = "228.5.6.7";
+        final int datagramPacketPort = 4444 ;
+        final int multicastPort = 4445 ;
+        final String networkInterface = "wlan0";
+
         // Create the multicast socket to receive messages from the master
-        MulticastSocket multicastSocket = new MulticastSocket(4445);
-        InetAddress multicastGroup = InetAddress.getByName("228.5.6.7");
+        MulticastSocket multicastSocket = new MulticastSocket(multicastPort);
+        InetAddress multicastGroup = InetAddress.getByName(multicastAddress);
         // Specify the network interface if it is not choosing the right one by default
-        multicastSocket.setNetworkInterface(NetworkInterface.getByName("wlan0"));
+        multicastSocket.setNetworkInterface(NetworkInterface.getByName(networkInterface));
         multicastSocket.joinGroup(multicastGroup);
 
         // Create point to point socket to send messages to the master
@@ -34,20 +42,19 @@ public class Main {
             byte[] longTampon = new byte[8];
             DatagramPacket timePacket = new DatagramPacket(longTampon, longTampon.length);
             multicastSocket.receive(timePacket);
-            long receivedValue =  bytesToLong(timePacket.getData());
-            System.out.println("Received master time : " + sdf.format(new Date(receivedValue)));
+            long receivedMasterTime =  bytesToLong(timePacket.getData());
+            System.out.println("Received master time : " + sdf.format(new Date(receivedMasterTime)));
 
             // Calculate new shift
             long currentTime = System.currentTimeMillis();
-            shift = currentTime - receivedValue;
+            shift = currentTime - receivedMasterTime;
 
             // Send new shift
-
             longTampon = ByteBuffer.allocate(Long.SIZE / Byte.SIZE).putLong(shift).array();
 
             //String s = timePacket.getAddress().getHostAddress();
             InetAddress address = InetAddress.getByName(timePacket.getAddress().getHostAddress());
-            DatagramPacket shiftPacket = new DatagramPacket(longTampon, longTampon.length, address,  4444);
+            DatagramPacket shiftPacket = new DatagramPacket(longTampon, longTampon.length, address,  datagramPacketPort);
             pointToPointSocket.send(shiftPacket);
             System.out.println("Shift sent : " + shift + " ms");
 
@@ -57,12 +64,16 @@ public class Main {
             shift = bytesToLong(newShiftPacket.getData()) - currentTime;
             System.out.println("New shift received : " + shift + " ms");
         }
-
-        //multicastSocket.leaveGroup(multicastGroup);
-        //multicastSocket.close();
-        //pointToPointSocket.close();
     }
 
+    /**
+     * name : bytesToLong
+     * Author : Antoine Drabble & Simon Baehler
+     * Date : 22.09.2016
+     * Description : transforme a data of bytes to a long
+     * param : byte[] bytes - data of bytes
+     * out : long - value of the data of bytes in a long format
+     */
     private static long bytesToLong(byte[] bytes){
         long result = 0;
         for (int i = 0; i < bytes.length; i++)
